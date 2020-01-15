cef4d47d5fb80b4eedc4b78caa3d5a7e899dce70
Hanna Lindgren <han.lind@gmail.com>
2020-01-15 11:02:45 +0100

    ahotts_testcall.py

--- ahotts_testcall.py
+++ ahotts_testcall.py
@@ -11,13 +11,23 @@ url="http://" + host +":" + port + "/ahotts_getaudio"
 data={'text': text, 'lang': 'eu', 'voice': {'lang': 'eu', 'name': 'ahotts-eu-female', 'engine': 'ahotts', 'adapter': 'adapters.ahotts_adapter'}, 'speed': '200'}
 response1 = requests.post(url, data=data)
 
-files=response1.json()
-wavfile=files['wav']
-wrdfile=files['wrd']
+if response1.status_code==200:
+    
+    files=response1.json()
+    wavfile=files['wav']
+    wrdfile=files['wrd']
+    
+    response2=requests.get("http://" + host + ":" + port + "/ahotts_downloadfile?file="+wavfile)
+    if response1.status_code==200:
+        
+        sys.stderr.write("%s\n" % response2)
+        
+        print("wav/"+wavfile)
+    else:
+        print("FAIL", response2)
+        sys.exit(1)
 
-response2=requests.get("http://" + host + ":" + port + "/ahotts_downloadfile?file="+wavfile)
-
-sys.stderr.write("%s\n" % response2)
-
-print("wav/"+wavfile)
+else:
+    print("FAIL", response1)
+    sys.exit(1)
 
